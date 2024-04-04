9d7dfdcc07f3e5fd2059f5b752d0facd41006ad2
GuiiAffonso <guiiaffonso.rh@gmail.com>
2024-04-04 20:29:37 -0300

    Resolve questão 2

--- array_e_indice.py
+++ array_e_indice.py
@@ -1,3 +1,8 @@
+# 1. Crie uma função que declara um array com 4 nomes diferentes e imprime cada um deles um embaixo do outro. Ao final faça um commit no GitHub.
+
+# Exemplo de entrada: Nenhuma Exemplo de saída:
+
+# 1-João 2-Maria 3-Fulano 4-Ciclano
 def mostra_nomes():
     nomes = ["João", "Maria", "Fulano", "Ciclano"]
     print("1 -", nomes[0])
@@ -6,3 +11,14 @@ def mostra_nomes():
     print("4 -", nomes[3])
 
 mostra_nomes()
+
+# 2. Crie uma função que declara um array com 4 nomes diferentes e imprime o primeiro e último nome do array. Ao final faça um commit no GitHub.
+
+# Exemplo de entrada : Nenhuma Exemplo de saída: 1-João 4-Beltrano
+
+def mostra_nome_especifico():
+    nomes = ["Tatiana", "Lorane", "Otávio", "Gabriel"]
+    print("2 -> ", nomes[0])
+    print("4 -> ", nomes[3])
+
+mostra_nome_especifico
