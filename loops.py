8941d741be9cc84afded1f2c39286044430f21a7
GuiiAffonso <guiiaffonso.rh@gmail.com>
2024-04-04 21:27:38 -0300

    Cria arq loops.py

---
+++ loops.py
@@ -0,0 +1,8 @@
+# 5. Crie uma função que imprime uma contagem de 0 a 20. Ao final faça um commit no GitHub.
+def contador():
+    contador = 0
+    while (contador <= 20):
+        print(contador)
+        contador = contador + 1
+
+contador()
