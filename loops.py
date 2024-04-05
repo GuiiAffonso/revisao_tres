d8ad6c9a31f0694a854205e008868726aeacd8dd
GuiiAffonso <guiiaffonso.rh@gmail.com>
2024-04-04 21:36:28 -0300

    Resolve questão 6

--- loops.py
+++ loops.py
@@ -5,4 +5,25 @@ def contador():
         print(contador)
         contador = contador + 1
 
-contador()
+# contador()
+
+# 6. Crie uma função que imprime uma contagem de 0 até o número que o usuário digitou. Ao final faça um commit no GitHub.
+#Solução while:
+def contador_personalizado():
+    numero_escolhido = int(input("Digite um número: "))
+
+    cont = 0
+    while (cont <= numero_escolhido):
+        print(cont)
+        cont = cont + 1
+
+# contador_personalizado()
+
+#Solução for:
+def contador_personalizado():
+    numero_escolhido = int(input("Digite um número: "))
+
+    for i in range(numero_escolhido):
+        print(i)
+
+# contador_personalizado()
